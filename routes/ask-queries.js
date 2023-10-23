6b147059ede352f88e3525194093baaef51d7c45
lj-D-coder <lj_1990@live.com>
2023-10-23 19:58:59 +0530

    worrking controller

--- routes/ask-queries.js
+++ routes/ask-queries.js
@@ -1,41 +1,9 @@
 import express from 'express';
-import OpenAI from 'openai';
-import dotenv from 'dotenv';
-dotenv.config();
-//import {Users} from '../models/usersModel.js';
+import {ask_queries} from '../controllers/askQueriesController.js';
 
 const askQueries = express.Router();
 
-const openai_api_key = process.env.OPENAI_API_KEY;
-
-
-const openai = new OpenAI({
-    apiKey: openai_api_key
-  });
-  
-  
-  askQueries.post('/', async (req, res) => {
-    console.log(req.body);
-    const userMessage = req.body.content;
-  
-    try {
-      const chat = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: userMessage }],
-        model: 'gpt-3.5-turbo',
-      });
-      console.log(chat.choices[0].message.content);
-    //   const newUser = {
-    //     name: "noname",
-    //     email: chat.choices[0].message.content,
-    //     phoneNo: 12345611,
-    // };
-    // const user = await Users.create(newUser);
-      res.status(200).json({ data: { response: chat.choices[0].message.content } ,statusCode: 1, error:"No error"});
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred while processing your request.' });
-    }
-  });
+  askQueries.post('/',ask_queries);
 
 
   export default askQueries;
