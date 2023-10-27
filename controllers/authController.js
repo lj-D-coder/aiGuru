0f3e7c7de7d7fc5c672b0a2d29f96197855fe535
lj-D-coder <lj_1990@live.com>
2023-10-27 08:38:38 +0530

    sign up api fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,17 +7,17 @@ export const signup = async (req, res, next) => {
     //console.log(req.body);
     try {
         if(
-            !req.body.username || 
             !req.body.email || 
             !req.body.password
         ) {
             return response.status(400).json(
                 {
-                    message: "send all required feilds: username, email, password",
+                    message: "send all required feilds: email, password",
                 });
         }
 
-        const { username, email, password } = req.body;
+        const { email, password } = req.body;
+        const username = email.split('@')[0];
         const hashedPassword = bcryptjs.hashSync(password, 10);
         const newUser = new Users({ username, email, password: hashedPassword });
         const user = await newUser.save();
@@ -26,11 +26,11 @@ export const signup = async (req, res, next) => {
         return res.status(201).json({
             data: { userId: user.id, email:user.email, userName: user.username },
             statusCode: 201, error: "successfully created"
-        });
+        });  
         
     } catch (error) {
         // console.log(error.message);
-        // error.keyValue["message"] = "already exist";
+        error.keyValue["message"] = "already exist";
         res.status(500).json(error);
         //next(errorHandler(400,"bad request"));
     }
@@ -68,18 +68,11 @@ export const saveGoogleinfo = async (req, res, next) => {
                 message: "send all required feilds: email, gToken, expiry",
             });
     }
-    // console.log(req.body);
-    // 
-    // const checkEmail = await Users.findOne({ email });
-    // const username = email.split('@')[0];
-
-    // console.log(username)
 
     const { email, gToken, expiry } = req.body;
     try {
         const checkEmail = await Users.findOne({ email });
         const username = email.split('@')[0];
-        //console.log(username);
         if (!checkEmail) {
             const saveUserInfo = new Users({ username, email, gAuthToken: gToken, expiry });
             const userInfo = await saveUserInfo.save();
