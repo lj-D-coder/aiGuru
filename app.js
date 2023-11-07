b8751a2348935c9f708ebd87ae49f82cd3e333a1
lj-D-coder <lj_1990@live.com>
2023-11-07 21:51:03 +0530

    tset

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const io = new Server(server);
         //arr_answer.push(chunk.choices[0].delta.content);
           console.log(chunk.choices[0].delta.content)
           socket.emit('answer-stream', () => {
-              chunk.choices[0].delta.content;
+              "this is a emit test ";
         })
         //res.write(`data: ${JSON.stringify(chunk.choices[0].delta.content)}\n\n`);
       }
