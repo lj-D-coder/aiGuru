5062006a466b23a3e939dda0e1225bcf4ca39f49
Jake L <143929593+lj-D-coder@users.noreply.github.com>
2024-05-12 19:53:56 +0530

    removing log

--- controllers/nodeMailerController.js
+++ controllers/nodeMailerController.js
@@ -4,7 +4,6 @@ import { TempUsers } from "../models/tempUserModel.js";
 
 dotenv.config();
 const password = process.env.MAIL_PASS;
-console.log(password)
 
 const transporter = nodemailer.createTransport({
   host: "mail.bestaitutorapp.com",
